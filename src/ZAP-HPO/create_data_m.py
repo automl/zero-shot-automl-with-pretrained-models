8f198484c09d2a8a557494fdbec9b408838bc284
Dipti Sengupta <diptisengupa@Diptis-MBP-2.fritz.box>
2022-05-06 01:19:24 +0200

    Run script
    download, upzip meta dataset if needed
    progress bar
    added ranks

--- src/ZAP-HPO/create_data_m.py
+++ src/ZAP-HPO/create_data_m.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 from pandas import json_normalize
 from glob import glob
+import tqdm
 import time
 from config2vector import get_config_response, create_searchspace
 
@@ -17,27 +18,33 @@ def load_df():
 
 
 # construct-> based on the meta features, per meta feature line in the meta-features.csv, merge metafeatures, perf matrix and config yaml.
-def construct_csv(meta_feat, perf_mat, incumbent_list):
+def construct_csv(meta_feat, perf_mat, incumbent_list, save_as):
     # converting to dicts for easier handling
     meta_dict=[value for value in meta_feat.to_dict('index').values()]
     data_csv=[]
     # Can it be faster??
-    for features in meta_dict:
+    for features in tqdm.tqdm(meta_dict):
         # accuracy for testing on features['dataset] by incumbent eg  accuracy.loc[[incumbent]]
         for incumbent in incumbent_list:
-            print('Finding for incumbent {} and test dataset {}'.format(incumbent, features['dataset']))
+            # print('Finding for incumbent {} and test dataset {}'.format(incumbent, features['dataset']))
             features['incumbent_of'] = incumbent
-            accuracy= perf_mat.loc[[features['dataset']]][incumbent]
-            features['accuracy'] =accuracy.values.item() # value returns as a series
+            accuracy_list = perf_mat.loc[[features['dataset']]]
+            accuracy= accuracy_list [incumbent].values.item()
+            # -1 because rank sets the lowest to 1 and data_m.csv uses 0. False so highest score is ranked 1st
+            ranks=accuracy_list.rank(axis=1, ascending=False,method='first')-1
+            # value returns as a series
+            features['accuracy'] =accuracy
+
             # open the relevant yaml from the incumbent, (replace to get paths right)
             yaml_path = os.path.join(args.metadata_path,'configs/kakaobrain_optimized_per_icgen_augmentation', incumbent.replace('-','/')+'.yaml' )
             hyperParams=create_searchspace(yaml_path)
             features={**features, **hyperParams}
+            features['ranks'] = int(ranks[incumbent].values.item())
             data_csv.append(features)
 
     data_df = pd.DataFrame(data_csv)
     data_df.set_index(['dataset'])
-    data_df.to_csv('test_me.csv',index=False)
+    data_df.to_csv(save_as,index=False)
 
     print('stop here')
 
@@ -48,17 +55,18 @@ if __name__=="__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Generate data_m.csv')
     parser.add_argument('--seed', type=int, default=2)
-    parser.add_argument('--metadata-path', type=str, default='/Users/diptisengupa/Downloads/meta_dataset', help='the path of meta_dataset directory')
-    parser.add_argument('--save-path', type=str, default='../data', help='save generated data_m.csv to this location')
+    parser.add_argument('--metadata-path', type=str, default='meta_dataset', help='the path of meta_dataset directory')
+    parser.add_argument('--save-path', type=str, default='data', help='save generated data_m.csv to this location')
+    parser.add_argument('--save-file', type=str, default='data_m_gen.csv', help='name the generated data_m.csv')
     args = parser.parse_args()
 
     # load_df()
 
-    meta_features=pd.read_csv(os.path.join(args.metadata_path,'meta_features.csv'))
+    meta_features=pd.read_csv(os.path.join(os.getcwd(),args.metadata_path,'meta_features.csv'))
     meta_features.columns.values[0] = "dataset"
     top_config= os.path.join(args.metadata_path,'configs/kakaobrain_optimized_per_icgen_augmentation')
     all_yaml_paths, incumbent_of, perf_matrix = get_config_response(config_dir=top_config, response_dir=os.path.join(args.metadata_path,'perf_matrix.csv'))
-    construct_csv(meta_features, perf_matrix, incumbent_of)
+    construct_csv(meta_features, perf_matrix, sorted(incumbent_of), args.save_file)
     end = time.time()
     hours, rem = divmod(end - start, 3600)
     minutes, seconds = divmod(rem, 60)
