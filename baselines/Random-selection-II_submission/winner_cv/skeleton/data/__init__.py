f5bcaee4cbb4eff8a5de916ab6192846e11875e1
Dipti Sengupta <diptisengupa@Diptis-MacBook-Pro-2.local>
2022-08-02 09:55:06 +0200

    Remove Old baselines

--- baselines/Random-selection-II_submission/winner_cv/skeleton/data/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# pylint: disable=wildcard-import
-from __future__ import absolute_import
-
-from . import augmentations
-from .dataloader import FixedSizeDataLoader, InfiniteSampler, PrefetchDataLoader
-from .dataset import TFDataset, TransformDataset, prefetch_dataset
-from .stratified_sampler import StratifiedSampler
-from .transforms import *
--- baselines/Random-selection-II_submission/winner_cv/skeleton/projects/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# pylint: disable=wildcard-import
-from __future__ import absolute_import
-
-from .api import *
-from .logic import *
-from .others import *
-
